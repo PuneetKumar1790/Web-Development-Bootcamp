d52b0011fd65f8347a557f0087cd84df1188e44e
Puneet Kumar <puneetk49081@gmail.com>
2024-07-21 13:33:58 +0000

    env commited

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+require('dotenv').config()
 console.log("Chai aur code")
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/twitter',(req,res)=>{
+  res.send('Puneetdotcom')
 })
+app.get('/login',(req,res)=>{
+  res.send('<h1>Please login at chai aur code</h1>')
+})
+app.get('/youtube',(req,res)=>{
+  res.send('<h2>Dhruv rathee roast MODI JI </h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
